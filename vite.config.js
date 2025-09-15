4c53a9c74aeb79352e6b3f520e99988890959b65
Akshaykumar Malpani <akshaykumarmalpani@Akshaykumars-MacBook-Air.local>
2025-09-16 00:56:17 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/prompt-generator/", // 👈 replace with your GitHub repo name
+  base: "/prompt-generator/",
 });
